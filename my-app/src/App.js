3f3dbe3d87346c50be456c18d42d52f3139516a7
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-05-20 23:13:05 +0500

    watch

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+// import UserList from './Components/UserLIst';
+import UserList2 from './Components/UserList2';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      {/*<Userlist />*/}
+      <UserList2 />
+
+
+    </div>
+  );
+}
+
+export default App;
